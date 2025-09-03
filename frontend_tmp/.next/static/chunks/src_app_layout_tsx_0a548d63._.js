1f266395b8b7c811b8f6e21665aba41dcf0bbefc
Panaghia George <38526106+panaghiageorge@users.noreply.github.com>
2025-09-03 18:20:18 +0300

    Add frontend app as regular folder

--- frontend_tmp/.next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0f0ba101._.css",
-  "static/chunks/node_modules_5edb3375._.js",
-  "static/chunks/src_app_ApolloProvider_tsx_48cb43bf._.js"
-],
-    source: "dynamic"
-});
